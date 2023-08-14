991802c366728996c36b555abb924d266f927616
duqian <duqian42707@163.com>
2023-08-15 00:09:43 +0800

    feat(all): 调通Java版关卡求解算法

--- sokoban-server/src/main/java/com/dqv5/sokoban/utils/SolveUtils.java
+++ sokoban-server/src/main/java/com/dqv5/sokoban/utils/SolveUtils.java
@@ -38,14 +38,16 @@ public class SolveUtils {
     public SolveUtils(List<BlockEntity> data) {
 
         map = new Integer[(BOUND_X + 1) * (BOUND_Y + 1)];
+        Arrays.fill(map, 0);
         data.stream().filter(t -> t.getType() == BlockType.WALL)
                 .forEach(t -> {
                     map[(t.getRow() + 1) * BOUND_X + (t.getCol() + 1)] = TREE;
                 });
 
-        LinkedHashSet allBuckets = data.stream()
+        LinkedHashSet<Long> allBuckets = data.stream()
                 .filter(b -> b.getType() == BlockType.BOX || b.getType() == BlockType.BOX_ON_GOAL)
-                .map(b -> (b.getRow() + 1) * BOUND_X + (b.getCol() + 1)).collect(Collectors.toCollection(() -> new LinkedHashSet()));
+                .map(b -> new Long((b.getRow() + 1) * BOUND_X + (b.getCol() + 1)))
+                .collect(Collectors.toCollection(() -> new LinkedHashSet()));
 
         spots = data.stream().filter(b -> b.getType() == BlockType.GOAL || b.getType() == BlockType.BOX_ON_GOAL || b.getType() == BlockType.MAN_ON_GOAL)
                 .map(s -> (s.getRow() + 1) * BOUND_X + (s.getCol() + 1))
@@ -60,8 +62,8 @@ public class SolveUtils {
 
 
         // 以两位Uint32来表示所有的8个buckets，以一位Uint32来表示x,y,direction，以一位Uint32来表示preIndex
-        int[] list = new int[256 * 1024 * 1024];
-        int[] packed = pack(allBuckets, x, y, 0, -1);
+        long[] list = new long[256 * 1024 * 1024];
+        long[] packed = pack(allBuckets, x, y, 0, -1);
         list[0] = packed[0];
         list[1] = packed[1];
         list[2] = packed[2];
@@ -74,8 +76,10 @@ public class SolveUtils {
             if (list[i] == 0) {
                 break;
             }
+            log.trace("unpack1:{}", list[i]);
             JSONObject jsonObject = unpack(list[i], list[i + 1], list[i + 2], list[i + 3]);
-            Set<Integer> buckets = (Set<Integer>) jsonObject.get("buckets");
+            log.trace("unpack:{}", jsonObject);
+            Set<Long> buckets = (Set<Long>) jsonObject.get("buckets");
             int x0 = jsonObject.getInteger("x");
             int y0 = jsonObject.getInteger("y");
             if (bfs(list, buckets, i, x0, y0, LEFT)
@@ -96,17 +100,17 @@ public class SolveUtils {
         }
     }
 
-    private boolean isEmpty(Set<Integer> buckets, int idx) {
-        return Objects.equals(map[idx], EMPTY) && !buckets.contains(idx);
+    private boolean isEmpty(Set<Long> buckets, long idx) {
+        return Objects.equals(map[(int) idx], EMPTY) && !buckets.contains(idx);
     }
 
-    private boolean isBucketInSpot(int idx) {
+    private boolean isBucketInSpot(long idx) {
         return spots.stream().anyMatch(spot -> spot == idx);
     }
 
 
-    private boolean deadBucket(Set<Integer> buckets, Integer bucket) {
-        Set<Integer> fixed = new HashSet<>();
+    private boolean deadBucket(Set<Long> buckets, Long bucket) {
+        Set<Long> fixed = new HashSet<>();
 
         boolean movable = canMove(buckets, fixed, bucket);
         if (movable) return false;
@@ -116,13 +120,13 @@ public class SolveUtils {
         return true;
     }
 
-    private boolean isImmovable(Set<Integer> buckets, Set<Integer> fixed, int idx) {
-        return map[idx] == TREE || map[idx] == null
+    private boolean isImmovable(Set<Long> buckets, Set<Long> fixed, long idx) {
+        return map[(int) idx] == TREE || map[(int) idx] == null
                 || buckets.contains(idx) && !canMove(buckets, fixed, idx);
     }
 
 
-    private boolean canMove(Set<Integer> buckets, Set<Integer> fixed, int idx) {
+    private boolean canMove(Set<Long> buckets, Set<Long> fixed, long idx) {
         if (fixed.contains(idx)) {
             return false;
         }
@@ -143,7 +147,7 @@ public class SolveUtils {
         return true;
     }
 
-    private Integer testMove(Set<Integer> buckets, Integer nextnext, Integer next) {
+    private Integer testMove(Set<Long> buckets, Long nextnext, Long next) {
         buckets.remove(next);
         buckets.add(nextnext);
         if (deadBucket(buckets, nextnext)) {
@@ -155,38 +159,38 @@ public class SolveUtils {
     }
 
 
-    private int[] makeDirectionXY(int direction, int x, int y, int delta) {
+    private long[] makeDirectionXY(int direction, int x, int y, int delta) {
         if (direction == 0) {
-            return new int[]{x - delta, y};
+            return new long[]{x - delta, y};
         }
         if (direction == 1) {
-            return new int[]{x + delta, y};
+            return new long[]{x + delta, y};
         }
         if (direction == 2) {
-            return new int[]{x, y - delta};
+            return new long[]{x, y - delta};
         }
         if (direction == 3) {
-            return new int[]{x, y + delta};
+            return new long[]{x, y + delta};
         }
         return null;
     }
 
-    private Integer getPos(int x, int y) {
+    private Long getPos(long x, long y) {
         if (x < 0 || x >= BOUND_X) return null;
         if (y < 0 || y >= BOUND_Y) return null;
         return y * BOUND_X + x;
     }
 
 
-    private int makeMove(Set<Integer> buckets, int x, int y, int direction) {
-        int[] pos = makeDirectionXY(direction, x, y, 1);
-        int next = getPos(pos[0], pos[1]);
+    private int makeMove(Set<Long> buckets, int x, int y, int direction) {
+        long[] pos = makeDirectionXY(direction, x, y, 1);
+        long next = getPos(pos[0], pos[1]);
         if (next > 0 && isEmpty(buckets, next)) {
             return MOVE_MAN;
         }
         if (next > 0 && buckets.contains(next)) {
             pos = makeDirectionXY(direction, x, y, 2);
-            int nextnext = getPos(pos[0], pos[1]);
+            long nextnext = getPos(pos[0], pos[1]);
             if (nextnext > 0 && isEmpty(buckets, nextnext)) {
                 return testMove(buckets, nextnext, next);
             }
@@ -194,59 +198,65 @@ public class SolveUtils {
         return 0;
     }
 
-    private boolean checkSpots(Set<Integer> buckets) {
-        return buckets.containsAll(spots);
+    private boolean checkSpots(Set<Long> buckets) {
+        return spots.stream().allMatch(x -> buckets.contains(new Long(x)));
     }
 
-    private int[] getRecord(int b1, int b2) {
+    private long[] getRecord(long b1, long b2) {
         Object b1Value = recordSet.get(b1);
-        if (b1Value == null) return null;
-        return ((Map<Integer, int[]>) b1Value).get(b2);
+        if (b1Value == null) {
+            return null;
+        }
+        long[] values = ((Map<Long, long[]>) b1Value).get(b2);
+        if (values == null) {
+            log.error("null");
+        }
+        return values;
     }
 
-    private void setRecord(int b1, int b2, int pos) {
-        int[] poss = getRecord(b1, b2);
+    private void setRecord(long b1, long b2, long pos) {
+        long[] poss = getRecord(b1, b2);
         if (poss == null) {
-            poss = new int[]{0, 0, 0, 0};
+            poss = new long[]{0, 0, 0, 0};
         }
         setPos(poss, pos);
         if (recordSet.get(b1) == null) {
             recordSet.put(b1, new HashMap<>());
         }
-        ((Map<Integer, int[]>) recordSet.get(b1)).put(b2, poss);
+        ((Map<Long, long[]>) recordSet.get(b1)).put(b2, poss);
     }
 
 
-    private boolean hasPos(int[] poss, int pos) {
+    private boolean hasPos(long[] poss, long pos) {
         int idx = 0;
         while (pos >= 30) {
             pos -= 30;
             idx++;
         }
-        return (poss[idx] & (1 << pos)) == 1;
+        return (poss[idx] & (1L << pos)) != 0;
     }
 
-    private void setPos(int[] poss, int pos) {
+    private void setPos(long[] poss, long pos) {
         int idx = 0;
         while (pos >= 30) {
             pos -= 30;
             idx++;
         }
-        poss[idx] |= (1 << pos);
+        poss[idx] |= (1L << pos);
     }
 
 
-    private int[] pack(Set<Integer> buckets, int x, int y, int step, int pre) {
-        int[] arr = new int[4];
-        List<Integer> sortedBuckets = buckets.stream().sorted().collect(Collectors.toList());
+    private long[] pack(Set<Long> buckets, long x, long y, int step, int pre) {
+        long[] arr = new long[4];
+        List<Long> sortedBuckets = buckets.stream().sorted().collect(Collectors.toList());
         for (int i = 0; i < 8; i++) {
-            Integer bucket;
+            Long bucket;
             if (i >= sortedBuckets.size()) {
-                bucket = 0xff;
+                bucket = 0xffL;
             } else {
                 bucket = sortedBuckets.get(i);
             }
-            if (bucket == null) bucket = 0xff;
+            if (bucket == null) bucket = 0xffL;
             if (i < 4) {
                 arr[0] |= bucket << (8 * i);
             } else {
@@ -259,12 +269,17 @@ public class SolveUtils {
             arr[2] |= step << 16;
         }
         arr[3] = pre;
-        log.debug("pack,x:{},y:{},step:{},pre:{},arr:{}", x, y, step, pre, arr);
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < 0) {
+                arr[i] = 4294967296L + arr[i];
+            }
+        }
+//        log.info("pack:{},{},{},{},{},{}", buckets, x, y, step, pre, arr);
         return arr;
     }
 
-    private JSONObject unpack(int a, int b, int c, int d) {
-        int[] buckets = new int[]{
+    private JSONObject unpack(long a, long b, long c, long d) {
+        long[] buckets = new long[]{
                 a & 0xff,
                 (a >>> 8) & 0xff,
                 (a >>> 16) & 0xff,
@@ -274,14 +289,14 @@ public class SolveUtils {
                 (b >>> 16) & 0xff,
                 (b >>> 24) & 0xff,
         };
-        Set<Integer> collect = Arrays.stream(buckets)
+        Set<Long> collect = Arrays.stream(buckets)
                 .filter(bb -> bb != 0xff)
                 .boxed()
                 .collect(Collectors.toSet());
 
-        int x = c & 0xff;
-        int y = (c >> 8) & 0xff;
-        int step = -1;
+        long x = c & 0xff;
+        long y = (c >> 8) & 0xff;
+        long step = -1;
         if (d != 0xffffffff) {
             step = (c >> 16) & 0xff;
         }
@@ -294,32 +309,38 @@ public class SolveUtils {
         return jsonObject;
     }
 
-    private Integer[] getSteps(int[] list, int idx) {
+    private Integer[] getSteps(long[] list, int idx) {
+        if (idx < 0) {
+            return new Integer[0];
+        }
         JSONObject data = unpack(list[idx], list[idx + 1], list[idx + 2], list[idx + 3]);
-        if (data.getInteger("step") >= 0) {
-            Integer[] pres = getSteps(list, data.getInteger("pre"));
-            List<Integer> list1 = Arrays.asList(pres);
+        log.trace("getSteps:{}", data);
+        if (data.getLong("step") >= 0) {
+            // intValue
+            Integer[] pres = getSteps(list, data.getLong("pre").intValue());
+            List<Integer> list1 = new ArrayList<>(Arrays.asList(pres));
             list1.add(data.getInteger("step"));
             return list1.toArray(new Integer[0]);
         }
         return new Integer[0];
     }
 
-    private boolean bfs(int[] list, Set<Integer> buckets, int i, int x, int y, int direction) {
-        Set<Integer> mvb = new HashSet<>(buckets);
+    private boolean bfs(long[] list, Set<Long> buckets, int i, int x, int y, int direction) {
+        Set<Long> mvb = new HashSet<>(buckets);
         int check = makeMove(mvb, x, y, direction);
-        log.debug("check:{}", check);
         if (check > 0) {
-            int[] pos = makeDirectionXY(direction, x, y, 1);
+            long[] pos = makeDirectionXY(direction, x, y, 1);
             if (check == MOVE_BOX && checkSpots(mvb)) {
                 Integer[] st = getSteps(list, i);
-                results = Arrays.asList(st);
+                results = new ArrayList<>(Arrays.asList(st));
                 results.add(direction);
                 return true;
             }
-            int[] packed = pack(mvb, pos[0], pos[1], direction, i);
-            int[] poss = getRecord(packed[0], packed[1]);
-            if (poss != null) {
+            log.trace("pack1:{},{},{},{},{}", mvb, pos[0], pos[1], direction, i);
+            long[] packed = pack(mvb, pos[0], pos[1], direction, i);
+            log.trace("packed:{}", packed);
+            long[] poss = getRecord(packed[0], packed[1]);
+            if (poss == null) {
                 list[count] = packed[0];
                 list[count + 1] = packed[1];
                 list[count + 2] = packed[2];
@@ -327,7 +348,7 @@ public class SolveUtils {
                 count += 4;
                 setRecord(packed[0], packed[1], getPos(pos[0], pos[1]));
             } else {
-                int idx = getPos(pos[0], pos[1]);
+                long idx = getPos(pos[0], pos[1]);
                 if (!hasPos(poss, idx)) {
                     list[count] = packed[0];
                     list[count + 1] = packed[1];
